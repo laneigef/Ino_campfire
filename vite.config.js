b97606f84af3ab4485e818759aeb2e46426e7e49
la_neige <laneigef@gmail.com>
2023-06-17 11:54:33 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl';
+
+export default defineConfig({
+    plugins: [
+        basicSsl(),
+    ],
+	server: {
+        https: true,
+        host: '0.0.0.0',
+        port: 8080,
+    },
+})
